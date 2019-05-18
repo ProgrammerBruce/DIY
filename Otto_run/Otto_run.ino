6c22dbf4e52545e6f4c94aea7d20bd25f87d0e79
Programmer Bruce <ProgrammerBruce@gmail.com>
2019-05-17 20:05:54 -0500

    Clean includes
    Change code format

--- Otto_run/Otto_run.ino
+++ Otto_run/Otto_run.ino
@@ -3,11 +3,15 @@
 //-----------------------------------------------------------------
 //-- Otto will run fast straight forward with this code!
 //-----------------------------------------------------------------
-#include <Servo.h> 
+#include <Arduino.h>
+#include <Servo.h>
+
 #include <Oscillator.h>
 #include <US.h>
 #include <Otto.h>
-Otto Otto;  //This is Otto!
+
+Otto otto;  //This is Otto!
+
 //---------------------------------------------------------
 //-- First step: Make sure the pins for servos are in the right position
 /*
@@ -20,10 +24,11 @@ YR 3==> |               | <== YL 2
 RR 5==>   -----   ------  <== RL 4
          |-----   ------|
 */
-  #define PIN_YL 2 //servo[2]
-  #define PIN_YR 3 //servo[3]
-  #define PIN_RL 4 //servo[4]
-  #define PIN_RR 5 //servo[5]
+#define PIN_YL 2 //servo[2]
+#define PIN_YR 3 //servo[3]
+#define PIN_RL 4 //servo[4]
+#define PIN_RR 5 //servo[5]
+
 ///////////////////////////////////////////////////////////////////
 //-- Global Variables -------------------------------------------//
 ///////////////////////////////////////////////////////////////////
@@ -33,30 +38,35 @@ int moveId=0;            //Number of movement
 int moveSize=15;         //Asociated with the height of some movements
 //---------------------------------------------------------
 bool obstacleDetected = false;
+
 ///////////////////////////////////////////////////////////////////
 //-- Setup ------------------------------------------------------//
 ///////////////////////////////////////////////////////////////////
-void setup(){
+void setup() {
   //Set the servo pins
-  Otto.init(PIN_YL,PIN_YR,PIN_RL,PIN_RR,true);
-  Otto.sing(S_connection); //Otto wake up!
-  Otto.home();
+  otto.init(PIN_YL,PIN_YR,PIN_RL,PIN_RR,true);
+  otto.sing(S_connection); //Otto wake up!
+  otto.home();
   delay(50);
-  Otto.sing(S_happy); // a happy Otto :)
+  otto.sing(S_happy); // a happy Otto :)
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  flashWithDelay(5);
 }
+
 ///////////////////////////////////////////////////////////////////
 //-- Principal Loop ---------------------------------------------//
 ///////////////////////////////////////////////////////////////////
 void loop() {
-
-               Otto.walk(2,500,1); //change T for lower value to run faster!
-                              
-             delay(50); }
+  flashWithDelay(2);
+  otto.walk(2,500,1); //change T for lower value to run faster!
+  delay(50);
+}
          
 ///////////////////////////////////////////////////////////////////
 //-- Function to read distance sensor & to actualize obstacleDetected variable
-void obstacleDetector(){
-   int distance = Otto.getDistance();
+void obstacleDetector() {
+   int distance = otto.getDistance();
         if(distance<15){
           obstacleDetected = true;
         }else{
